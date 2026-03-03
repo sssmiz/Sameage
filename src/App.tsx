d3fae0c8d89d47a90e13e60eaa5c134062047fc8
Mizuki <mizuki01093211@gmail.com>
2026-03-03 11:15:53 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Sameage">
         <AppProvider>
           <Particles />
           <UserInfoBar />
